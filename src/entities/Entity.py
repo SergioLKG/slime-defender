f291412189200212608678dd1ca39b7b90196237
2dama <silleda03@gmail.com>
2024-01-12 17:31:48 +0100

    Entity

--- src/entities/Entity.py
+++ src/entities/Entity.py
@@ -1,14 +1,16 @@
+import os
+
 import pygame
 
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, vida, x, y, size=50, element="Neutro"):
-        super().__init__()
-
-        self.vida = vida
+    def __init__(self, x, y, size=(50, 50), element="neutro"):
+        pygame.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
         self.size = size
         self.element = element
-        self.image = self.load_image()
+        self.image = self.load_image("assets/entities", "debug.png")
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
@@ -18,39 +20,33 @@ class Entity(pygame.sprite.Sprite):
         self.barra_vida_alto = 5
 
     def tabla_tipos(self, cantidad, elemento_enemigo):
-        if self.element is "Neutro":
+        if self.element is "neutro":
             cantidad = cantidad
         elif self.element == elemento_enemigo:
             cantidad //= 2  # Daño a la mitad elemento
-        elif elemento_enemigo is "Agua" and self.element is "Fuego":
+        elif elemento_enemigo is "agua" and self.element is "fuego":
             cantidad *= 2  # Daño x2 elemento
-        elif elemento_enemigo is "Hielo" and self.element is "Agua":
+        elif elemento_enemigo is "hielo" and self.element is "agua":
             cantidad *= 2  # Daño x2 elemento
-        elif elemento_enemigo is "Tierra" and self.element is "Hielo":
+        elif elemento_enemigo is "tierra" and self.element is "hielo":
             cantidad *= 2  # Daño x2 elemento
         return cantidad
 
-    def load_image(self):
-        # Cargar la imagen correspondiente al elemento
-        # (Implementación específica según tus necesidades)
-        image = pygame.Surface((self.size, self.size))
-        image.fill((255, 150, 255))  # Color blanco por defecto
-        return image
+    def load_image(self, directorio, nombre):
+        ruta = os.path.join(directorio, nombre)
+        try:
+            image = pygame.image.load(ruta)
+            image = pygame.transform.scale(image, size=self.size)
+            return image.convert_alpha()
+        except FileNotFoundError:
+            print("Error! no se puede cargar la imagen")
 
-    def recibir_dano(self, cantidad, elemento_enemigo="Neutro"):
-        cantidad = self.tabla_tipos(cantidad, elemento_enemigo)
-        if self.vida <= 0:
-            self.morir()
+    def recibir_dano(self, cantidad, elemento_enemigo="neutro"):
+        pass  # Clase reemplazable
 
     def morir(self):
         print("Una entidad ha muerto.")
-
-    def dibujar_barra_vida(self, screen):
-        barra_vida_rect = pygame.Rect(self.rect.x, self.rect.y - 10, self.barra_vida_ancho, self.barra_vida_alto)
-        pygame.draw.rect(screen, (255, 0, 0), barra_vida_rect, 0)
-        vida_actual_rect = pygame.Rect(self.rect.x, self.rect.y - 10,
-                                       max(0, int(self.barra_vida_ancho * (self.vida / 100))), self.barra_vida_alto)
-        pygame.draw.rect(screen, self.barra_vida_color, vida_actual_rect, 0)
+        self.kill()
 
     def atacar(self, objetivo):
         pass
